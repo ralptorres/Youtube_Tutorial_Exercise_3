8d4d9ce8c70540ff9205c40f51e5f24c688ce5e1
ralptorres <ralp.torres@gmail.com>
2024-08-27 22:33:04 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let girl = "Alenere";
+let boy = "David";
+let pronoun = "she";
+let drink = "coffee";
+
+alert(
+  `${girl}, the friendly neighbor, waved at ${boy} as ${pronoun} walked by ${boy}'s house. ${boy} smiled back and invited ${girl} in for a cup of ${drink}.`
+);
